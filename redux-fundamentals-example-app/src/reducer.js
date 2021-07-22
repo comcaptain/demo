132e99e8b26f87b85ae19e1288d2e77801c73c15
Tony <htmlcaptain@gmail.com>
2021-07-22 21:02:37 +0900

    Similarly, create filtersSlice

--- redux-fundamentals-example-app/src/reducer.js
+++ redux-fundamentals-example-app/src/reducer.js
@@ -13,27 +13,7 @@ const initialState = {
 // Use the initialState as a default value
 export default function appReducer(state = initialState, action)
 {
-	// The reducer normally looks at the action type field to decide what happens
-	switch (action.type)
-	{
-		case 'filters/statusFilterChanged': {
-			return {
-				// Copy the whole state
-				...state,
-				// Overwrite the filters value
-				filters: {
-					// copy the other filter fields
-					...state.filters,
-					// And replace the status field with the new value
-					status: action.payload
-				}
-			}
-		}
-
-		// Do something here based on the different types of actions
-		default:
-			// If this reducer doesn't recognize the action type, or doesn't
-			// care about this specific action, return the existing state unchanged
-			return state
-	}
+	// If this reducer doesn't recognize the action type, or doesn't
+	// care about this specific action, return the existing state unchanged
+	return state
 }

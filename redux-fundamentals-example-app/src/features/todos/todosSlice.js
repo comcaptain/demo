19556d73e801800b1d637333235f5566e3a3a07b
Tony <htmlcaptain@gmail.com>
2021-07-22 21:00:57 +0900

    Move todo related reduction logic to todosSlice

--- redux-fundamentals-example-app/src/features/todos/todosSlice.js
+++ redux-fundamentals-example-app/src/features/todos/todosSlice.js
@@ -14,6 +14,32 @@ export default function todosReducer(state = initialState, action)
 {
 	switch (action.type)
 	{
+		case 'todos/todoAdded': {
+			// Can return just the new todos array - no extra object around it
+			return [
+				...state,
+				{
+					id: nextTodoId(state),
+					text: action.payload,
+					completed: false
+				}
+			]
+		}
+		case 'todos/todoToggled': {
+			return state.map(todo =>
+			{
+				if (todo.id !== action.payload)
+				{
+					return todo
+				}
+
+				return {
+					...todo,
+					completed: !todo.completed
+				}
+			})
+		}
+
 		default:
 			return state
 	}

88823caafd5bb796bc9ff28f01b2cfd3e4e6eebd
Tony <htmlcaptain@gmail.com>
2022-09-11 09:20:51 +0900

    Update

--- angular14-demo/src/app/app.component.ts
+++ angular14-demo/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'angular14-demo';
+export class AppComponent
+{
+	title = 'Tour of Heros';
 }
